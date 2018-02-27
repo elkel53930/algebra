4a13aa45ae1a7131c691143674c7e153cb2c107f
elkel53930 <elkel53930@gmail.com>
2018-02-27 19:28:56 +0900

    *1 や +0の簡約に対応

--- src/Derivative.hs
+++ src/Derivative.hs
@@ -13,7 +13,7 @@ derivExp :: String -> Symbol ->  Either String String
 derivExp src s =
   let result = parse pExpression "" src
   in  case result of
-        Right exp -> Right . oExpression $ deriv exp s
+        Right exp -> Right . oExpression . simplify $ deriv exp s
         Left  err -> Left $ show err
 
 deriv :: Expression -> Symbol -> Expression
@@ -22,24 +22,10 @@ deriv (Variable v) s =
   if v == s
     then Number 1
     else Number 0
-deriv (Sum e1 e2) s = makeSum (deriv e1 s) (deriv e2 s)
+deriv (Sum e1 e2) s = Sum (deriv e1 s) (deriv e2 s)
 deriv (Product e1 e2) s =
-  makeSum (makeProduct e1 $ deriv e2 s )
-          (makeProduct e2 $ deriv e1 s )
-
-makeSum :: Expression -> Expression -> Expression
-makeSum e1 e2 =
-  case (isZero e1, isZero e2) of
-    (True, True) -> Number 0
-    (True, False) -> e2
-    (False, True) -> e1
-    (False, False) -> Sum e1 e2
-
-makeProduct :: Expression -> Expression -> Expression
-makeProduct e1 e2 =
-  case isZero e1 || isZero e2 of
-    True -> Number 0
-    False -> Product e1 e2
+  Sum (Product e1 $ deriv e2 s )
+      (Product e2 $ deriv e1 s )
 
 isZero :: Expression -> Bool
 isZero (Number x) = x == 0
@@ -49,13 +35,27 @@ isZero (Product e1 e2) = (isZero e1) || (isZero e2)
 
 simplify :: Expression -> Expression
 simplify e@(Sum e1 e2) =
-  case (simplify e1, simplify e2) of
-    (Number x1, Number x2) -> Number $ x1+x2
-    otherwise -> e
+  let
+    s1 = simplify e1
+    s2 = simplify e2
+  in
+    case (s1, s2) of
+      (Number 0, er)         -> er
+      (el, Number 0)         -> el
+      (Number x1, Number x2) -> Number $ x1+x2
+      otherwise -> Sum s1 s2
 simplify e@(Product e1 e2) =
-  case (simplify e1, simplify e2) of
-    (Number x1, Number x2) -> Number $ x1*x2
-    otherwise -> e
+  let
+    s1 = simplify e1
+    s2 = simplify e2
+  in
+    case (s1, s2) of
+      (Number 1, er)         -> er
+      (el, Number 1)         -> el
+      (Number 0, _)          -> Number 0
+      (_, Number 0)          -> Number 0
+      (Number x1, Number x2) -> Number $ x1*x2
+      otherwise -> Product s1 s2
 simplify e = e
 
 pExpression :: Parser Expression
